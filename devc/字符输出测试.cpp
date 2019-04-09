74e9f6516b64b95ed2026b939b23790c9e5caf5a
dodoshuai <1115112505@qq.com>
2019-04-09 23:31:30 +0800

    Revert "1.15"
    This reverts commit ac5bf8dbc412d51080b800b8351cd86d1a17df81.

--- devc/字符输出测试.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char a=3;
-	char b=4;
-	printf("%c %c\n",a,b);
-} 
--- devc/测试2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include"book.h"
-using namespace std;
-int main()
-{
-	book A;
-	A.print();
-	return 0;
-}
