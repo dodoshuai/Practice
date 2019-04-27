75e46ddea50747787460685d24c139cb99047726
dodoshuai <1115112505@qq.com>
2019-04-27 22:28:40 +0800

    suanfan 1

---
+++ leetco/快速并集加权版.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define N 10000
+int main(){
+	int i,j,p,q,id[N],sz[N];
+	for(int i=0;i<N;i++){
+		id[i]=i;sz[i]=1;
+	}
+	while(scanf("%d %d\n",&p,&q)==2){
+		for(int i=p;i!=id[i];i=id[i]);
+		for(int j=q;j!=id[j];j=id[j]);
+		if(i==j) continue;
+		if(sz[i]<sz[j]){
+			id[i]=j;sz[j]+=sz[i];
+		}
+		else {
+			id[j]=i;sz[i]+=sz[j];
+		}
+		printf(" %d %d\n",p,q);
+	}
+	return 0;
+}
