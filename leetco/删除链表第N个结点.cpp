399aabc4852d4a59b5da5b7b4eabd9cc76136d27
dodoshuai <1115112505@qq.com>
2019-04-29 23:53:51 +0800

    note is count backwards  num

--- leetco/删除链表第N个结点.cpp
+++ leetco/删除链表第N个结点.cpp
@@ -5,11 +5,11 @@
 ListNode *removeNthFromEnd(ListNode* head,int n){
 	ListNode *cur=head;
 	while(n--)
-		cur=cur->next;	
+		cur=cur->next;	//  find  n
 	if(!cur)
-		return head->next;
+		return head->next;//when n   is   NULL     so    (ex:n=2;   1,2,3    find 2)
 	ListNode *last=head;
-	while(cur->next!=NULL){
+	while(cur->next!=NULL){// go  head  when  head is NULL
 		last=last->next;
 		cur=cur->next;
 	}
