377ac47f4c724437ee344542c8fea8d160d6f4f8
dodoshuai <1115112505@qq.com>
2019-05-07 00:00:47 +0800

    cengxubianli

---
+++ leetco/层序遍历.cpp
@@ -0,0 +1,21 @@
+vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;//二维数组
+        queue<TreeNode*> nums;//循环队列
+        nums.push(root);//将根节点加入队列
+        if(root==NULL)//将root为空排除
+            return res;
+        while(!nums.empty()){//当队列不为空，进入循环
+            vector<int> num;//num数组
+            queue<TreeNode*> nextnodes;//保存下一层队列
+            while(!nums.empty()){//当队列不为空，进入循环
+                TreeNode* tre = nums.front() ;//将队列头赋值
+                num.push_back(tre->val) ;//将其加入数组
+                nums.pop() ;  
+               if(tre->left) nextnodes.push(tre->left);
+                if(tre->right)nextnodes.push(tre->right);
+            }
+            nums=nextnodes;
+            res.push_back(num);
+        }
+        return res;
+    }
